40457144f3c77599d936a25ac3755ce73e179fa7
Layan003 <layansalman03@gmail.com>
2025-03-04 01:17:48 +0300

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='dark:bg-black dark:text-white h-full w-full'>
+      <h1 className='text-3xl font-bold underline text-primary'>Hello</h1>
+    </section>
+  )
+}
+
+export default App
